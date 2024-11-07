d38bfb91cd2ca8076f3525691639d4182862f465
AvinashKumar <avikr3799@gmail.com>
2024-11-07 18:14:17 +0530

    added solution of print height of stack

---
+++ Recurision-Questions/HeightofStack.java
@@ -0,0 +1,21 @@
+
+public class HeightofStack {
+    public static int heightOfStack(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        if (n % 2 == 0) {
+            return heightOfStack(x, n/2) * heightOfStack(x, n/2);
+        }else{
+            return heightOfStack(x, n/2) * heightOfStack(x, n/2) * x;
+        }
+    }
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        int ans = heightOfStack(x, n);
+        System.out.println(ans);
+    }
+}
