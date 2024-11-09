3fa206c41c515beb1b89825238ee4c2ce7c8fea4
AvinashKumar <avikr3799@gmail.com>
2024-11-09 10:14:50 +0530

    added Multilevel inheritance in Java QUESTION

--- Tree-Questions/MaximumDepthofBinaryTree.java
+++ Tree-Questions/MaximumDepthofBinaryTree.java
@@ -25,9 +25,8 @@ public class MaximumDepthofBinaryTree {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
         root.right = new TreeNode(20);
-        root.left.left = new TreeNode(15);
-        root.left.right = new TreeNode(7);
-        root.left.left.left = new TreeNode(16);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
 
         int depth = maxDepth(root);
         System.out.println("Maximum Depth of the Binary Tree: " + depth);
